40f436fdfafe15303445397d9484474d931d13f0
Sy <elitonjr1@gmail.com>
2025-07-19 14:20:07 -0300

    changing address to backlog

--- finance-front/src/services/api.js
+++ finance-front/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5110/api",
+  baseURL: "https://financecontrol-f6x1.onrender.com",
 });
 
 // carregar token do localStorage (se existir)
